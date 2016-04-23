7aa5901ff13fb58b67ca594c38664350e549fba1
Richard Hull <rm_hull@yahoo.co.uk>
2016-04-24 00:10:49 +0100

    Rework to use ring's piped-input-stream

--- src/ring_cede/core.clj
+++ src/ring_cede/core.clj
@@ -1,25 +1,23 @@
 (ns ring-cede.core
   (:require
+    [ring.util.io :refer [piped-input-stream]]
     [ring.util.response :refer [response content-type]]
     [compojure.response :refer [Renderable]])
   (:import
     [javax.imageio ImageIO]
     [java.awt.image RenderedImage]
-    [java.io OutputStream ByteArrayInputStream ByteArrayOutputStream]))
+    [java.io OutputStream]))
 
-(defn- create-pipe [f pipe-size]
-  (with-open [out-stream (ByteArrayOutputStream. pipe-size)]
-    (f out-stream)
-    (ByteArrayInputStream. (.toByteArray out-stream))))
 
 (defn- png-writer [^RenderedImage img ^OutputStream out]
-  (ImageIO/write img "png" out))
+  (ImageIO/write img "png" out)
+  (.flush out))
 
 (extend-protocol Renderable
   RenderedImage
   (render [this _]
     (->
       (partial png-writer this)
-      (create-pipe 0x20000)
+      piped-input-stream
       response
       (content-type "image/png"))))
